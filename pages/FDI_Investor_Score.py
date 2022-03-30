6a7c8ecac3eb657b0a11d91ba62be8a7d5f89a4a
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-03-30 15:51:04 +0900

    ver 0.6

--- pages/FDI_Investor_Score.py
+++ pages/FDI_Investor_Score.py
@@ -2,8 +2,5 @@ import streamlit as st
 import pandas as pd
 from collections import Counter
 def app():
-    unicorns = pd.read_excel('asset/UnicornsSince2010.xlsx')
-    unicorn_investors = [_ for sublist in unicorns['Institutional Investors'] if type(sublist) is str for _ in sublist.split('\n')]
-    unicorn_investors_count = Counter(unicorn_investors).most_common()
-    df = pd.DataFrame(unicorn_investors_count)
-    st.dataframe(df)
+    unicorns = pd.read_excel('asset/Top100Candidate_Investment_History.xlsx')
+    st.dataframe(unicorns)
