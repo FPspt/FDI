d0ddf7e6251be2950a9faf28adf2bfc87bd348d9
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-03-30 15:26:33 +0900

    updater ver0.5

--- pages/FDI_Investor_Score.py
+++ pages/FDI_Investor_Score.py
@@ -3,7 +3,7 @@ import pandas as pd
 from collections import Counter
 def app():
     unicorns = pd.read_excel('asset/UnicornsSince2010.xlsx')
-    unicorn_investors = [_ for sublist in unicorns['Institutional Investors'] if type(_) is str for _ in sublist.split('\n')]
+    unicorn_investors = [_ for sublist in unicorns['Institutional Investors'] if type(sublist) is str for _ in sublist.split('\n')]
     unicorn_investors_count = Counter(unicorn_investors).most_common()
     df = pd.DataFrame(unicorn_investors_count)
     st.dataframe(df)
