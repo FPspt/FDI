2786afbf76cff9273bc1bb5b8d67d185effacf08
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-03-30 15:51:55 +0900

    update ver0.7

--- pages/FDI_Investor_Score.py
+++ pages/FDI_Investor_Score.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 from collections import Counter
 def app():
-    unicorns = pd.read_excel('asset/Top100Candidate_Investment_History.xlsx')
+    unicorns = pd.read_excel('asset/Top100Candidate_Investment_History.xlsx').astype(str)
     st.dataframe(unicorns)
