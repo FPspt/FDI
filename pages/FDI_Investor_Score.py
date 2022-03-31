62f90c406dc3c5740afe38e97b01c33aedddd612
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-03-31 10:18:17 +0900

    update ver0.14

--- pages/FDI_Investor_Score.py
+++ pages/FDI_Investor_Score.py
@@ -6,5 +6,5 @@ def app():
         st.markdown(f.read())
 
     unicorns = pd.read_excel('asset/A_22H1_Investor Score.xlsx').astype(str)
-    unicorns = unicorns.style.set_properties(**{'background-color': 'red'}, subset=['Final Score'])
+    unicorns = unicorns.style.set_properties(**{'background-color': 'mediumturquoise'}, subset=['Final Score'])
     st.dataframe(unicorns)
