75819bce3ed5d41ed2f2e5645ef2d06cc02898db
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-03-31 18:03:00 +0900

    update ver0.68

--- FDI_app.py
+++ FDI_app.py
@@ -8,12 +8,9 @@ st.set_page_config(layout="wide")
 
 st.title("Future Disruption Index")
 
-lower_bound = datetime(2019,1,1)
-upper_bound = datetime(2022,3,1)
-
 app.add_page("Future Disruption Index", FDI_Main.app)
 app.add_page("FDI Feed Score", FDI_Feed_Score.app)
-app.add_page(" -> FDI Co-Occurence Calculator", FDI_CoOccurence.app(lower_bound,upper_bound))
+app.add_page(" -> FDI Co-Occurence Calculator", FDI_CoOccurence.app)
 app.add_page("FDI Investor Score",FDI_Investor_Score.app)
 app.add_page("Cleanse File",FDI_Cleanser.app)
 #app.add_page("FDI Cleanser", FDI_Cleanser.app)
