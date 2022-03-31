f706ce1d1b0d7ff3e778f5e7c6b3de4d7f17fcfc
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-03-31 11:16:13 +0900

    update ver0.15

---
+++ pages/FDI_Feed_Score.md
@@ -0,0 +1,2 @@
+
+FDI Feed Score provides an overview of emerging feeds by tracking top_n investors.
--- pages/FDI_Feed_Score.py
+++ pages/FDI_Feed_Score.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import pandas as pd
 
 def app():
-    st.write('About to be added')
+    with open("pages/FDI_Feed_Score.md") as f:
+        st.markdown(f.read())
+
+    col1,col2,col3 = st.beta_columns([3,1,1])
+    with col1:
+        st.write('About to be added.')
+    with col2:
+        top_n = st.slider('Top_N Investors', 0, 100, 10)
+    with col3:
+        top_n = st.slider('Top_N Investors', 0, 100, 10)
