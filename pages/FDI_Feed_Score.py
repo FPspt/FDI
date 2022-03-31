4d37d541d674c496b53f36684160a1dce50cf327
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-03-31 12:50:25 +0900

    updater ver0.26

--- pages/FDI_Feed_Score.py
+++ pages/FDI_Feed_Score.py
@@ -12,7 +12,7 @@ def app():
     col1,col2,col3 = st.columns([2,1,1])
     with col1:
         col1_df = calculate_feed_score_ratio(data, score, 100)
-        col1_df.drop('Top_100_Occurence')
+        #col1_df.drop('Top_100_Occurence')
         #col1_df = col1_df.style.set_properties(**{'background-color': 'mediumturquoise'}, subset=[f'Top_100_Ratio'])
 
         st.dataframe(col1_df)
@@ -24,8 +24,8 @@ def app():
     with col2:
         top_n_col2 = st.slider('Top_N Investors', 0, 100, 10, key='col2_slider')
         col2_df = calculate_feed_score_ratio(data, score, top_n_col2)
-        col2_df.drop(f'Top_{top_n_col2}_Occurence')
-        col2_df = col2_df.style.set_properties(**{'background-color': 'mediumturquoise'}, subset=[f'Top_{top_n_col2}_Ratio'])
+        #col2_df.drop(f'Top_{top_n_col2}_Occurence')
+        #col2_df = col2_df.style.set_properties(**{'background-color': 'mediumturquoise'}, subset=[f'Top_{top_n_col2}_Ratio'])
 
         st.dataframe(col2_df)
 
@@ -35,8 +35,8 @@ def app():
     with col3:
         top_n_col3 = st.slider('Top_N Investors', 0, 100, 60, key='col3_slider')
         col3_df = calculate_feed_score_ratio(data, score, top_n_col3)
-        col3_df.drop(f'Top_{top_n_col3}_Occurence')
-        col3_df = col3_df.style.set_properties(**{'background-color': 'mediumturquoise'}, subset=[f'Top_{top_n_col3}_Ratio'])
+        #col3_df.drop(f'Top_{top_n_col3}_Occurence')
+        #col3_df = col3_df.style.set_properties(**{'background-color': 'mediumturquoise'}, subset=[f'Top_{top_n_col3}_Ratio'])
 
         st.dataframe(col3_df)
 
