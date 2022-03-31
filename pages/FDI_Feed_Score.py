92da6d42aa5ce2b04597bc94c3aa6edc72badd5f
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-03-31 11:17:12 +0900

    update ver0.16

--- pages/FDI_Feed_Score.py
+++ pages/FDI_Feed_Score.py
@@ -5,10 +5,10 @@ def app():
     with open("pages/FDI_Feed_Score.md") as f:
         st.markdown(f.read())
 
-    col1,col2,col3 = st.beta_columns([3,1,1])
+    col1,col2,col3 = st.columns([3,1,1])
     with col1:
         st.write('About to be added.')
     with col2:
-        top_n = st.slider('Top_N Investors', 0, 100, 10)
+        top_n_col2 = st.slider('Top_N Investors', 0, 100, 10)
     with col3:
-        top_n = st.slider('Top_N Investors', 0, 100, 10)
+        top_n_col3 = st.slider('Top_N Investors', 0, 100, 10)
